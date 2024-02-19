77aad97df8444eae4f2a0a5d5bb1e6f91a637234
10000001a <pgdn06288888@gmail.com>
2024-02-19 23:16:48 +0900

    feat: User 엔티티에 AuditableEntity 적용

--- src/main/java/com/hongik/ce/f23/team1/screw/user/domain/User.java
+++ src/main/java/com/hongik/ce/f23/team1/screw/user/domain/User.java
@@ -1,5 +1,6 @@
 package com.hongik.ce.f23.team1.screw.user.domain;
 
+import com.hongik.ce.f23.team1.screw.global.domain.AuditableEntity;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -10,20 +11,23 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToOne;
+import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.AccessLevel;
 import lombok.ToString;
 import org.springframework.lang.Nullable;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
-@EqualsAndHashCode(of = {"id"})
+@EqualsAndHashCode(
+    of = {"id"},
+    callSuper = true
+)
 @ToString(of = {"id", "email", "nickname"})
-public class User {
+public class User extends AuditableEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
